f75b8e7cf82480e15036d798fca0d005752cc728
duxinyi <2580191658@.com>
2023-04-17 17:40:30 +0800

    docs: 加上第一个报表接口

--- src/views/main/cma/report1/report1.vue
+++ src/views/main/cma/report1/report1.vue
@@ -3,43 +3,43 @@
     <div class="select-two">
       <div class="system-select" v-for="item in selectData" :key="item.name">
         <span>{{ item.name }}:</span>
-        <el-select :popper-append-to-body="false" v-model="item.value" placeholder="請選擇">
+        <el-select clearable :popper-append-to-body="false" v-model="item.value" placeholder="請選擇">
           <el-option v-for="item in options[item.name]" :key="item.value" :label="item.label" :value="item.value">
           </el-option>
         </el-select>
       </div>
       <el-button class="btn" type="primary" round @click="getSearchData">Search</el-button>
     </div>
-    <!-- :cell-style="{ background: 'transparent', color: '#fff' }" -->
-
     <el-table
+      :row-style="{ height: '30px' }"
       :data="tableData"
       :header-cell-style="{ background: '#f8cbad', color: '#000' }"
       :cell-style="cellStyle"
+      height="calc(100% - 74.9px)"
     >
       <el-table-column
         v-for="(taT, index) in tableTitle"
         :key="index"
-        :width="taT.lable === 'Lens Vendor' ? '90' : ''"
-        prop="Device"
+        :width="taT.name === 'Lens Vendor' ? '90' : ''"
+        prop="value"
         align="center"
-        :label="taT.lable"
+        :label="taT.name"
       >
         <el-table-column
-          v-show="taT.children"
-          v-for="(c, index) in taT.children"
+          v-show="taT.chileColumn"
+          v-for="(c, index) in taT.chileColumn"
           :key="index"
-          prop="date"
+          prop="value"
           align="center"
-          :label="c.lable"
+          :label="c.name"
         >
           <el-table-column
-            v-show="c.son"
-            v-for="(s, index) in c.son"
+            v-show="c.chileColumn"
+            v-for="(s, index) in c.chileColumn"
             :key="index"
-            prop="c1"
+            prop="value"
             align="center"
-            :label="s"
+            :label="s.name"
           ></el-table-column>
         </el-table-column>
       </el-table-column>
@@ -48,6 +48,8 @@
 </template>
 
 <script>
+// 导入点击搜索数据
+import { GetReport1Search } from "@/api/cma/report1"
 export default {
   name: "report1",
   props: {},
@@ -55,29 +57,7 @@ export default {
   data() {
     return {
       // 表头名称
-      tableTitle: [
-        { lable: "Project" },
-        { lable: "Lens Vendor" },
-        { lable: "AA MC" },
-        {
-          lable: "06:00~Now累計生產狀況",
-          children: [
-            { lable: "AA Input" },
-            { lable: "FailRate" },
-            { lable: "Fail Rate", son: ["Para_A", "Para_B", "Para_C", "Para_D"] },
-            { lable: "Input", son: ["Para_A", "Para_B", "Para_C", "Para_D"] }
-          ]
-        },
-        {
-          lable: "近兩小時生產狀況",
-          children: [
-            { lable: "AA Input", son: ["AA Input"] },
-            { lable: "FailRate", son: ["FailRate"] },
-            { lable: "Fail Rate", son: ["Para_A", "Para_B", "Para_C", "Para_D"] },
-            { lable: "Input", son: ["Para_A", "Para_B", "Para_C", "Para_D"] }
-          ]
-        }
-      ],
+      tableTitle: [],
       // 下拉框值
       selectData: [
         { name: "DeviceSeries", value: "" },
@@ -87,388 +67,89 @@ export default {
       options: {
         DeviceSeries: [
           {
-            value: "选项1",
+            value: "MW-X",
             label: "MW-X"
           },
           {
-            value: "选项2",
-            label: "MW-X2"
+            value: "MW-F",
+            label: "MW-F"
           },
           {
-            value: "选项3",
-            label: "MW-X3"
+            value: "MW-E",
+            label: "MW-E"
           },
           {
-            value: "选项4",
+            value: "MW-X4",
             label: "MW-X4"
           },
           {
-            value: "选项5",
+            value: "MW-X5",
             label: "MW-X5"
+          },
+          {
+            value: "MW-X6",
+            label: "MW-X6"
+          },
+          {
+            value: "MW-X7",
+            label: "MW-X7"
+          },
+          {
+            value: "MW-X8",
+            label: "MW-X8"
+          },
+          {
+            value: "MW-X9",
+            label: "MW-X9"
           }
         ],
         Suppy: [
           {
-            value: "选项1",
+            value: "Genius",
             label: "Genius"
           },
           {
-            value: "选项2",
+            value: "Genius2",
             label: "Genius2"
           },
           {
-            value: "选项3",
+            value: "Genius3",
             label: "Genius3"
           },
           {
-            value: "选项4",
+            value: "Genius4",
             label: "Genius4"
           },
           {
-            value: "选项5",
+            value: "Genius5",
             label: "Genius5"
           }
         ]
       },
       // 表格数据
-      tableData: [
-        {
-          Device: "1",
-          Machine: "2",
-          Station: "3",
-          date: "5",
-          c1: "6",
-          c2: "6",
-          c3: "6",
-          c4: "6",
-          c5: "6",
-          c6: "6",
-          c7: "6",
-          c8: "6"
-        },
-        {
-          Device: "1",
-          Machine: "2",
-          Station: "3",
-          date: "5",
-          c1: "6",
-          c2: "6",
-          c3: "6",
-          c4: "6",
-          c5: "6",
-          c6: "6",
-          c7: "6",
-          c8: "6"
-        },
-        {
-          Device: "1",
-          Machine: "2",
-          Station: "3",
-          date: "5",
-          c1: "6",
-          c2: "6",
-          c3: "6",
-          c4: "6",
-          c5: "6",
-          c6: "6",
-          c7: "6",
-          c8: "6"
-        },
-        {
-          Device: "1",
-          Machine: "2",
-          Station: "3",
-          date: "5",
-          c1: "6",
-          c2: "6",
-          c3: "6",
-          c4: "6",
-          c5: "6",
-          c6: "6",
-          c7: "6",
-          c8: "6"
-        },
-        {
-          Device: "1",
-          Machine: "2",
-          Station: "3",
-          date: "5",
-          c1: "6",
-          c2: "6",
-          c3: "6",
-          c4: "6",
-          c5: "6",
-          c6: "6",
-          c7: "6",
-          c8: "6"
-        },
-        {
-          Device: "1",
-          Machine: "2",
-          Station: "3",
-          date: "5",
-          c1: "6",
-          c2: "6",
-          c3: "6",
-          c4: "6",
-          c5: "6",
-          c6: "6",
-          c7: "6",
-          c8: "6"
-        },
-        {
-          Device: "1",
-          Machine: "2",
-          Station: "3",
-          date: "5",
-          c1: "6",
-          c2: "6",
-          c3: "6",
-          c4: "6",
-          c5: "6",
-          c6: "6",
-          c7: "6",
-          c8: "6"
-        },
-        {
-          Device: "1",
-          Machine: "2",
-          Station: "3",
-          date: "5",
-          c1: "6",
-          c2: "6",
-          c3: "6",
-          c4: "6",
-          c5: "6",
-          c6: "6",
-          c7: "6",
-          c8: "6"
-        },
-        {
-          Device: "1",
-          Machine: "2",
-          Station: "3",
-          date: "5",
-          c1: "6",
-          c2: "6",
-          c3: "6",
-          c4: "6",
-          c5: "6",
-          c6: "6",
-          c7: "6",
-          c8: "6"
-        },
-        {
-          Device: "1",
-          Machine: "2",
-          Station: "3",
-          date: "5",
-          c1: "6",
-          c2: "6",
-          c3: "6",
-          c4: "6",
-          c5: "6",
-          c6: "6",
-          c7: "6",
-          c8: "6"
-        },
-        {
-          Device: "1",
-          Machine: "2",
-          Station: "3",
-          date: "5",
-          c1: "6",
-          c2: "6",
-          c3: "6",
-          c4: "6",
-          c5: "6",
-          c6: "6",
-          c7: "6",
-          c8: "6"
-        },
-        {
-          Device: "1",
-          Machine: "2",
-          Station: "3",
-          date: "5",
-          c1: "6",
-          c2: "6",
-          c3: "6",
-          c4: "6",
-          c5: "6",
-          c6: "6",
-          c7: "6",
-          c8: "6"
-        },
-        {
-          Device: "1",
-          Machine: "2",
-          Station: "3",
-          date: "5",
-          c1: "6",
-          c2: "6",
-          c3: "6",
-          c4: "6",
-          c5: "6",
-          c6: "6",
-          c7: "6",
-          c8: "6"
-        },
-        {
-          Device: "1",
-          Machine: "2",
-          Station: "3",
-          date: "5",
-          c1: "6",
-          c2: "6",
-          c3: "6",
-          c4: "6",
-          c5: "6",
-          c6: "6",
-          c7: "6",
-          c8: "6"
-        },
-        {
-          Device: "1",
-          Machine: "2",
-          Station: "3",
-          date: "5",
-          c1: "6",
-          c2: "6",
-          c3: "6",
-          c4: "6",
-          c5: "6",
-          c6: "6",
-          c7: "6",
-          c8: "6"
-        },
-        {
-          Device: "1",
-          Machine: "2",
-          Station: "3",
-          date: "5",
-          c1: "6",
-          c2: "6",
-          c3: "6",
-          c4: "6",
-          c5: "6",
-          c6: "6",
-          c7: "6",
-          c8: "6"
-        },
-        {
-          Device: "1",
-          Machine: "2",
-          Station: "3",
-          date: "5",
-          c1: "6",
-          c2: "6",
-          c3: "6",
-          c4: "6",
-          c5: "6",
-          c6: "6",
-          c7: "6",
-          c8: "6"
-        },
-        {
-          Device: "1",
-          Machine: "2",
-          Station: "3",
-          date: "5",
-          c1: "6",
-          c2: "6",
-          c3: "6",
-          c4: "6",
-          c5: "6",
-          c6: "6",
-          c7: "6",
-          c8: "6"
-        },
-        {
-          Device: "1",
-          Machine: "2",
-          Station: "3",
-          date: "5",
-          c1: "6",
-          c2: "6",
-          c3: "6",
-          c4: "6",
-          c5: "6",
-          c6: "6",
-          c7: "6",
-          c8: "6"
-        },
-        {
-          Device: "1",
-          Machine: "2",
-          Station: "3",
-          date: "5",
-          c1: "6",
-          c2: "6",
-          c3: "6",
-          c4: "6",
-          c5: "6",
-          c6: "6",
-          c7: "6",
-          c8: "6"
-        },
-        {
-          Device: "1",
-          Machine: "2",
-          Station: "3",
-          date: "5",
-          c1: "6",
-          c2: "6",
-          c3: "6",
-          c4: "6",
-          c5: "6",
-          c6: "6",
-          c7: "6",
-          c8: "6"
-        },
-        {
-          Device: "1",
-          Machine: "2",
-          Station: "3",
-          date: "5",
-          c1: "6",
-          c2: "6",
-          c3: "6",
-          c4: "6",
-          c5: "6",
-          c6: "6",
-          c7: "6",
-          c8: "6"
-        },
-        {
-          Device: "1",
-          Machine: "2",
-          Station: "3",
-          date: "5",
-          c1: "6",
-          c2: "6",
-          c3: "6",
-          c4: "6",
-          c5: "6",
-          c6: "6",
-          c7: "6",
-          c8: "6"
-        }
-      ]
+      tableData: []
     }
   },
   created() {
     this.$store.commit("fullLoading/SET_TITLE", "SFR不良率 by AA MC")
+    this.getData()
   },
   methods: {
     getSearchData() {
-      console.log(111)
+      let inputValue = this.selectData
+      this.getData(inputValue[0].value, inputValue[1].value)
+    },
+    async getData(devSer, spy) {
+      let res = await GetReport1Search(devSer, spy)
+      this.tableTitle = res.columns
+      this.tableData = res.rows[0]
+      console.log(res)
     },
     cellStyle(row, column, rowIndex, columnIndex) {
-      console.log("row", row)
-      console.log("column", column)
-      console.log("rowIndex", rowIndex)
-      console.log("columnIndex", columnIndex)
+      // console.log("row", row)
+      // console.log("column", column)
+      // console.log("rowIndex", rowIndex)
+      // console.log("columnIndex", columnIndex)
 
       if (row.Device !== row.Station && columnIndex === 13) {
         return { color: "red" }
@@ -489,7 +170,7 @@ export default {
   background: transparent;
   border: 1px solid #1683af;
   margin-top: 20px;
-  height: calc(100% - 74.9px);
+  // height: calc(100% - 74.9px);
   // overflow: auto;
 }
 ::v-deep .el-table tr {
@@ -531,6 +212,9 @@ export default {
   span {
     padding: 0 10px 0 25px;
   }
+}
+::v-deep .el-scrollbar {
+  width: 240px;
 }
 ::v-deep .el-input--suffix .el-input__inner {
   background-color: rgba(0, 0, 0, 0.3);
