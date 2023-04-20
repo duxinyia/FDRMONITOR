05c870d6aa2d2e63f0affe3424d2e0fbb5a2ddd3
duxinyi <2580191658@.com>
2023-04-20 17:35:39 +0800

    docs: 第一个报表修改接口参数

--- src/api/cma/report1.js
+++ src/api/cma/report1.js
@@ -3,5 +3,5 @@ import moment from "moment"
 let { post: $post } = request
 
 export let GetReport1Search = (deviceNo, supply) =>
-  // $post(`api/FPDCSFR/Search?SearchType=FPDC&Type=AAMC&DeviceNo=${deviceNo}&Supply=${supply}`)
-  $post(`api/FPDCSFR/SearchAAMC?DefectType=SFR&DeviceSeriers=MW&datetime=${moment().format("YYYY-MM-DD HH:mm:ss")}`)
+  // $post(`api/FPDCSFR/Search?SearchType=FPDC&Type=AAMC&DeviceNo=${deviceNo}&Supply=${supply}`)YYYY-MM-DD HH:mm:ss
+  $post(`api/FPDCSFR/SearchAAMC?DefectType=SFR&DeviceSeriers=MW&datetime=${moment().format("YYYY-MM-DD")}`)
